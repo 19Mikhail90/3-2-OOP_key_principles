48106a9e7239331ea69058616a34a8edd38052e5
Mikhail Gozhenko <sacred9900@gmail.com>
2021-09-19 13:44:33 +0500

    My version

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Post {
+    private String id;
+    private String labelGroupUrl;
+    private String nameGroup;
+    private int datePost;
+    private String imagePostUrl;
+    private String imageLabelFavoritesUrl;
+    private String textOnImage;
+
+    private String imageLabelLikeUrl;
+    public int like;
+
+    private String imageLabelCommentsUrl;
+    public String commentsInfo;
+
+    private String imageLabelRepostsUrl;
+    public int repostsInfo;
+
+    private String imageLabelViewsUrl;
+    public int Views;
+
+}
